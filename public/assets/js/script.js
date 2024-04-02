0c40a44117f19977cc4f006ed9f3837c0e8c49af
alecvipa <hebrit_626@hotmail.com>
2024-04-01 21:44:06 -0400

    added refris

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -94,4 +94,68 @@ span.onclick = function () {
     modalFour.style.display = "none";
 }
 
+// Get the modal Fifth
+var modalFifth = document.getElementById("myModalFifth");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgFifth = document.getElementById("myImgFifth");
+var modalImgFifth = document.getElementById("imgFifth");
+var captionTextFifth = document.getElementById("captionFifth");
+imgFifth.onclick = function () {
+    modalFifth.style.display = "block";
+    modalImgFifth.src = this.src;
+    captionTextFifth.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeFifth")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modalFifth.style.display = "none";
+}
+
+
+// Get the modal Sixth
+var modalSixth = document.getElementById("myModalSixth");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgSixth = document.getElementById("myImgSixth");
+var modalImgSixth = document.getElementById("imgSixth");
+var captionTextSixth = document.getElementById("captionSixth");
+imgSixth.onclick = function () {
+    modalSixth.style.display = "block";
+    modalImgSixth.src = this.src;
+    captionTextSixth.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeSixth")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modalSixth.style.display = "none";
+}
+
+// Get the modal Seventh
+var modalSeventh = document.getElementById("myModalSeventh");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgSeventh = document.getElementById("myImgSeventh");
+var modalImgSeventh = document.getElementById("imgSeventh");
+var captionTextSeventh = document.getElementById("captionSeventh");
+imgSeventh.onclick = function () {
+    modalSeventh.style.display = "block";
+    modalImgSeventh.src = this.src;
+    captionTextSeventh.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeSeventh")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modalSeventh.style.display = "none";
+}
+
 
